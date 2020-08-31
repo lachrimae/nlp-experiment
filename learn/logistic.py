f7801e72e0a05af6049c6c19d5794edd7ca323fd
Curran McConnell <curran@atidot.com>
2020-08-30 21:58:38 -0400

    logistic.py: improved comments

--- learn/logistic.py
+++ learn/logistic.py
@@ -4,15 +4,22 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
+# This is an easy way to turn our question, which is
+# about a pair of bools: (geography, biography)
+# into a single variable with four possible values.
+# This transformation is required in order to use multinomial
+# logistic regression.
 def assign_letter(bool_tuple):
     if not bool_tuple[0] and not bool_tuple[1]:
         return 'a'
-    elif bool_tuple[0] and not bool_tuple[1]:
+    if bool_tuple[0] and not bool_tuple[1]:
         return 'b'
-    elif not bool_tuple[0]:
+    if not bool_tuple[0] and bool_tuple[1]:
         return 'c'
-    else:
+    if bool_tuple[0] and bool_tuple[1]:
         return 'd'
+    # if this code is written incorrectly it should be noisy
+    raise Exception
 
 # In this function, I build two logistic regression models which try to predict whether
 # an article is about geography, or biography, respectively. 
@@ -27,9 +34,7 @@ def find_accuracy(df):
 
     regression = LogisticRegression(multi_class='multinomial')
     regression.fit(stats_train, categories_train)
-    categories_pred = regression.predict(stats_test)
+    categories_pred = regression.predict(stats_test:
     accuracy = metrics.accuracy_score(categories_test, categories_pred)
 
-    accuracy2 = metrics.accuracy_score(categories_test, ['a'] * len(categories_test))
-
-    return (accuracy2, regression.coef_)
+    return (accuracy, regression.coef_)
