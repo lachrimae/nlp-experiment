b0080b0d3ccd58c7d8ca2a0e5d6fb282e80d4d8a
Curran McConnell <curran@atidot.com>
2020-08-30 21:52:48 -0400

    finalized sklearn code

--- learn/logistic.py
+++ learn/logistic.py
@@ -3,17 +3,33 @@ from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+
+def assign_letter(bool_tuple):
+    if not bool_tuple[0] and not bool_tuple[1]:
+        return 'a'
+    elif bool_tuple[0] and not bool_tuple[1]:
+        return 'b'
+    elif not bool_tuple[0]:
+        return 'c'
+    else:
+        return 'd'
+
+# In this function, I build two logistic regression models which try to predict whether
+# an article is about geography, or biography, respectively. 
 def find_accuracy(df):
-    stats = None
-    categories = None
-    # initialize the training and testing sets, and fit the model.
+    stats     = df[['mean_word_length', 'mean_sentence_length', 'stddev_word_length', 'stddev_sentence_length']]
+    categories = df[['biography', 'geography']]
+
+    categories = list(map(assign_letter, zip(categories.biography, categories.geography)))
     stats_train, stats_test, categories_train, categories_test = train_test_split(
         stats, categories, random_state=4
     )
-    logistic_regression = LogisticRegression()
-    logistic_regression.fit(stats_train, categories_train)
 
-    # check the prediction for accuracy
-    categories_pred = logistic_regression.predict(stats_test)
+    regression = LogisticRegression(multi_class='multinomial')
+    regression.fit(stats_train, categories_train)
+    categories_pred = regression.predict(stats_test)
     accuracy = metrics.accuracy_score(categories_test, categories_pred)
-    return accuracy
+
+    accuracy2 = metrics.accuracy_score(categories_test, ['a'] * len(categories_test))
+
+    return (accuracy2, regression.coef_)
--- learn/main.py
+++ learn/main.py
@@ -1,4 +1,5 @@
 import logistic
+import pandas
 import neural
 from sqlalchemy import create_engine
 from sys import argv, exit
@@ -9,7 +10,7 @@ SELECT * FROM articles
 WHERE mean_word_length IS NOT NULL
 AND mean_sentence_length IS NOT NULL
 AND stddev_word_length IS NOT NULL
-AND stddev_sentence IS NOT NULL;
+AND stddev_sentence_length IS NOT NULL;
 """
 
 
@@ -19,7 +20,7 @@ def main():
         connect_string = f'postgresql+psycopg2://{argv[2]}:{argv[3]}@{argv[1]}/nlp_experiment'
     except IndexError as e:
         print('Use the format: main.py db_hostname db_username db_password')
-        raise e
+        exit(1)
 
     engine = create_engine(connect_string)
     with engine.connect() as connection:
@@ -27,9 +28,9 @@ def main():
             READ_NON_NULL_ENTRIES,
             connection
         )
-        logistic_accuracy = logistic.find_accuracy(articles)
-        neural_accuracy = neural.find_accuracy(articles)
-        print("Logistic accuracy was ", logistic_accuracy, "\nNeural accuracy was ", neural_accuracy)
+        logistic_accuracy_and_coeffs = logistic.find_accuracy(articles)
+        print("Logistic accuracy was", logistic_accuracy_and_coeffs[0])
+        print("Logistic coefficients were\n", logistic_accuracy_and_coeffs[1])
 
 
 if __name__ == '__main__':
